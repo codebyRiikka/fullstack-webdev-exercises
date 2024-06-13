03589dbd847b366e3c6ddf2e390d11f961a96641
Riikka-KaoPT23TIEd <rixuuu95@gmail.com>
2024-06-13 10:25:52 +0300

    Exercises 1.1 and 1.2 completed

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,59 @@
+// Component to display header: course name
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+// Component to display a single part's name and exercises count
+const Part = ({name, exercises}) => {
+  return (
+    <p>
+    {name} {exercises}
+    </p>
+  )
+}
+
+// Component to render the list of parts and their exercise counts
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+// Component to calculate and display all exercises
+const Total = ({parts}) => {
+  const allExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <p>Number of exercises {allExercises}</p>
+  )
+}
+
+// Main app component
+const App = () => {
+  // Course name definition
+  const course = 'Half Stack application development'
+  // Course parts definition - array with objects: name & exercise count
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10},
+    { name: 'Using props to pass data', exercises: 7},
+    { name: 'State of a component', exercises: 14},
+  ];
+
+  // Returns the structure of this application 
+  /* Rendering the Header, Content and Total components and passes the parts
+  arrays as a props */
+  return (
+    <div>
+      <Header course = {course} />
+      <Content parts = {parts} />
+      <Total parts = {parts} />
+    </div>
+  )
+}
+
+export default App
