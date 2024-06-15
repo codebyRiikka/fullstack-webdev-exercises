c5c415217945564fa32aeaedd25a2d10162e5321
Riikka-KaoPT23TIEd <rixuuu95@gmail.com>
2024-06-15 09:50:30 +0300

    Unicafe exercise 1.6 - 1.11 completed and a little css added to the app aswell.

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,61 @@
+// Fullstack exercises 1.6 - 1.11
+
+import { useState } from 'react'
+import './App.css';
+
+// Component to display one statisticline
+const StatisticLine = ({text, value}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
+// Component to show all statisticlines
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const posPercent = all === 0 ? 0 : (good / all) * 100
+
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="Good" value={good} />
+        <StatisticLine text="Neutral" value={neutral} />
+        <StatisticLine text="Bad" value={bad} />
+        <StatisticLine text="All" value={all} />
+        <StatisticLine text="Average" value={average} />
+        <StatisticLine text="Positive" value={posPercent + ' %'} />
+      </tbody>
+    </table>
+  )
+}
+
+// Component to display one statisticbutton + text on it
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+// Main component for the state and for rendering other components
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div className="container">
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} /> 
+    </div>
+  )
+}
+
+export default App
