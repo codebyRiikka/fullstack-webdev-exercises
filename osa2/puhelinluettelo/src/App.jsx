2d9c1942b5d451d6f59f3fd94e0da3e61974270c
Riikka-KaoPT23TIEd <rixuuu95@gmail.com>
2024-06-24 11:51:50 +0300

    Exercises 2.12-2.16 completed, work in progress for the part 2.17

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,17 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import personApi from '/src/personApi';
+import '/src/index.css';
+
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null;
+  }
+  return (
+    <div className={`notification ${type}`}>
+      {message}
+    </div>
+  )
+}
 
 // Component to filter the list of persons
 const Filter = ({ filter, filterChangeHandling}) => {
@@ -28,20 +40,22 @@ const ThePerson = ({ addPerson, newName, nameChangeHandling, newNumber, numberCh
 }
 
 // Component to display the list of persons
-const Persons = ({ persons }) => {
+const Persons = ({ persons, deletePerson }) => {
   return (
     <ul>
       {persons.map(person =>
-        <Person key={person.id} person={person} />
+        <Person key={person.id} person={person} deletePerson={deletePerson} />
       )}
     </ul>
   )
 }
 
 // Component to display a single person
-const Person = ({ person }) => {
+const Person = ({ person, deletePerson }) => {
   return (
-    <li>{person.name} {person.number}</li>
+    <li>{person.name} {person.number}
+    <button onClick={() => deletePerson(person.id)}>Delete</button>
+    </li>
   )
 }
 
@@ -51,13 +65,15 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [NotificationMessage, setNotificationMessage] = useState(null)
+  const [NotificationType, setNotificationType] = useState(null)
 
   // Effect hook to fetch data from JSON server
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data);
+    personApi
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons);
       })
       .catch(error => {
         console.error('Error fetching the initial data:', error);
@@ -70,18 +86,70 @@ const App = () => {
     const personToAdd = {
       name: newName,
       number: newNumber,
-      id: persons.length ? Math.max(persons.map(person => person.id)) + 1 : 1
     }
 
-    // Check if the person already exists
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already in this Phonebook`)
+    const existingPerson = persons.find(person => person.name === newName);
+    // Check if the person already exists, if exists: suggest user if they want to replace old number with the new one
+    if (existingPerson) {
+      const confirmUpdate = window.confirm(`${newName} is already in this Phonebook, replace the old number with a new one?`);
+      if (confirmUpdate) {
+        personApi
+          .update(existingPerson.id, personToAdd)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== existingPerson.id ? person : returnedPerson))
+            setNewName('')
+            setNewNumber('')
+            setNotificationMessage(`Updated ${newName}'s number`);
+            setNotificationType('edit');
+            setTimeout(() => {
+              setNotificationMessage(null)
+              setNotificationType(null)
+            }, 5000)
+          })
+          .catch(error => {
+            console.error('Error updating the person:', error);
+          })
+      }
     } else {
-      setPersons(persons.concat(personToAdd))
+      personApi
+        .create(personToAdd)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+          setNotificationMessage(`Added ${newName}`);
+          setNotificationType('success')
+          setTimeout(() => {
+            setNotificationMessage(null);
+            setNotificationType(null);
+        }, 5000)
+      })
+      .catch(error => {
+          console.error('Error adding a new person:', error);
+      })
     }
-    setNewName('')
-    setNewNumber('')
   } 
+
+  // Function to delete person
+  const deletePerson = (id) => {
+    const personToDelete = persons.find(person => person.id === id);
+    if(window.confirm('Are you sure you want to delete person from phonebook?')) {
+      personApi
+        .removeData(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+          setNotificationMessage(`Deleted ${personToDelete.name}`);
+          setNotificationType('deleted')
+          setTimeout(() => {
+            setNotificationMessage(null);
+            setNotificationType(null);
+        }, 5000)
+      })
+      .catch(error => {
+        console.error('Error deleting the person:', error);
+      })
+    }
+  }
 
   const nameChangeHandling = (event) => {
     setNewName(event.target.value)
@@ -102,6 +170,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={NotificationMessage} type={NotificationType}/>
       <Filter filter={filter} filterChangeHandling={filterChangeHandling} />
       <h3>Add a new</h3>
 
@@ -115,7 +184,7 @@ const App = () => {
 
       <h3>Numbers</h3>
       
-      <Persons persons={personsToShow} />
+      <Persons persons={personsToShow} deletePerson={deletePerson} />
     </div>
   )
 }
